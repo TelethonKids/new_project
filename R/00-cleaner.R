7f46cbc69968d6692fab26c942748e773902a577
paul.stevenson@telethonkids.org.au <paul.stevenson@telethonkids.org.au>
2021-02-09 09:08:15 +0800

    Updated main branch with time-saving data cleaning steps

--- R/00-cleaner.R
+++ R/00-cleaner.R
@@ -3,7 +3,7 @@
 #' Script to read in "raw-data", perform preliminary data cleaning/harmonisation
 #' steps and save in the "data" folder for munging.
 #' 
-#' Updated 11-Dec-2020 by Paul Stevenson
+#' Updated 9-Feb-2021 by Paul Stevenson
 #' 
 
 #### Project automation with ProjectTemplate ----
@@ -16,12 +16,28 @@ source("R/99-helper.R")
 
 #### load data ----
 
-dat_raw <- read_xlsx("data-raw/file.xlsx", sheet = "Sheet1", range = "A1:B2", col_types = c("text"))
 # dat_raw <- read_csv(file = "data-raw/file.csv", col_types = cols(.default = "c"))
+dat_raw <- read_xlsx("data-raw/file.xlsx", sheet = "Sheet1", col_types = c("text"))
+dict <- read_xlsx("data-raw/data_dictionary.xlsx", sheet = "Data Dictionary", col_types = c("text"))
 
 #### Data cleaning ----
 
+# clean column names
+
+names(dat_raw) <- map_chr(names(dat_raw), function(x, d) ifelse(x %in% d$name, d[d$name == x,]$name_clean, x), d = dict)
+
+# Set column class
+
+dat_raw <- dat_raw %>%
+  mutate(across(c("date", "dob", dict[dict$type %in% c("Integer", "Numeric"),]$name_clean), as.numeric),
+         across(c("date", "dob"), excel_numeric_to_date),
+         across("gender", factor, levels = c("M", "F"), labels = c("Male", "Female")))
+
+# Apply labels to columns
+
+var_label(dat_raw) <- map_chr(names(dat_raw), function(x, d) ifelse(x %in% d$name_clean, d[d$name_clean == x,]$description, x), d = dict)
 
 #### save data ----
 
 save(dat_raw, file = "data/dat.RData")
+cache("dict")
