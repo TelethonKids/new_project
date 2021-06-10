05108e1f84f426efbf1e6e4cc376ef113119323a
paul.stevenson@telethonkids.org.au <paul.stevenson@telethonkids.org.au>
2021-06-10 10:27:01 +0800

    Updates to basic branch

--- NEWS.md
+++
@@ -1,4 +0,0 @@
-# Project template
-
-* 2020-12-11 v1.0.0 Initial release
-* 2020-12-13 v1.0.0 Created "basic" branch
--- munge/00-cleaner.R
+++ munge/00-cleaner.R
@@ -3,7 +3,7 @@
 #' Script to read in "raw-data", perform preliminary data cleaning/harmonisation
 #' steps and save in the "data" folder for munging.
 #' 
-#' Updated 9-Feb-2020 by Paul Stevenson
+#' Updated 10-June-2020 by Paul Stevenson
 #' 
 
 #### Project automation with ProjectTemplate ----
@@ -16,28 +16,16 @@ source("R/99-helper.R")
 
 #### load data ----
 
-# dat_raw <- read_csv(file = "data/file.csv", col_types = cols(.default = "c"))
-dat_raw <- read_xlsx("data/file.xlsx", sheet = "Sheet1", col_types = c("text"))
-dict <- read_xlsx("data/data_dictionary.xlsx", sheet = "Data Dictionary", col_types = c("text"))
+# dat_raw <- read_csv(file = "data-raw/file.csv", col_types = cols(.default = "c"))
+dat_raw <- read_xlsx("data-raw/file.xlsx", sheet = "Sheet1", col_types = c("text"))
+
+# dictionary <- read_csv(file = "data-raw/data_dictionary.xlsx", col_types = cols(.default = "c"))
+dictionary <- read_xlsx("data-raw/data_dictionary.xlsx", sheet = "Data Dictionary", col_types = c("text"))
 
 #### Data cleaning ----
 
-# clean column names
-
-names(dat_raw) <- map_chr(names(dat_raw), function(x, d) ifelse(x %in% d$name, d[d$name == x,]$name_clean, x), d = dict)
-
-# Set column class
-
-dat_raw <- dat_raw %>%
-  mutate(across(c("date", "dob", dict[dict$type %in% c("Integer", "Numeric"),]$name_clean), as.numeric),
-         across(c("date", "dob"), excel_numeric_to_date),
-         across("gender", factor, levels = c("M", "F"), labels = c("Male", "Female")))
-
-# Apply labels to columns
-
-var_label(dat_raw) <- map_chr(names(dat_raw), function(x, d) ifelse(x %in% d$name_clean, d[d$name_clean == x,]$description, x), d = dict)
+# dat_raw <- biometrics::clean_REDCap(dat_raw, dictionary)
 
 #### save data ----
 
-save(dat_raw, file = "data/dat_raw.RData")
-cache("dict")
+save(dat_raw, dictionary, file = "data/dat.RData")
