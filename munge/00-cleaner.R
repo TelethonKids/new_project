82e191223e98c68d8c48652584bbc185e124de02
paul.stevenson@telethonkids.org.au <paul.stevenson@telethonkids.org.au>
2021-06-10 10:34:00 +0800

    Updates to main branch

---
+++ munge/00-cleaner.R
@@ -0,0 +1,31 @@
+#' R/00-cleaner.R
+#' 
+#' Script to read in "raw-data", perform preliminary data cleaning/harmonisation
+#' steps and save in the "data" folder for munging.
+#' 
+#' Updated 10-June-2021 by Paul Stevenson
+#' 
+
+#### Project automation with ProjectTemplate ----
+
+ProjectTemplate::load.project(munging = FALSE, cache_loading = FALSE, data_loading = FALSE)
+
+#### helper functions ----
+
+source("R/99-helper.R")
+
+#### load data ----
+
+# dat_raw <- read_csv(file = "data-raw/file.csv", col_types = cols(.default = "c"))
+dat_raw <- read_xlsx("data-raw/file.xlsx", sheet = "Sheet1", col_types = c("text"))
+
+# dat_raw <- read_csv(file = "data-raw/data_dictionary.csv", col_types = cols(.default = "c"))
+dict <- read_xlsx("data-raw/data_dictionary.xlsx", sheet = "Data Dictionary", col_types = c("text"))
+
+#### Data cleaning ----
+
+# dat_raw <- clean_REDCap(dat_raw, dictionary)
+
+#### save data ----
+
+save(dat_raw, dictionary, file = "data/dat.RData")
